a67b5673ea34a1b98ad818f6ea33459a7b3f539a
berenger-bayer <somminberengersannibayer@gmail.com>
2024-08-08 13:11:55 -0500

    navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { NavMenu } from "./_components/navbar";
 import { SideBar } from "./_components/sidebar";
+import { Separator } from "@/components/ui/separator";
+import { SidebarNav } from "./(preference)/forms/components/sidebar-nav";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -10,7 +12,28 @@ export const metadata: Metadata = {
   title: "Projet 2 Hbi",
   description: "Un site web comme leboncoin",
 };
-
+const sidebarNavItems = [
+  {
+    title: "Profile",
+    href: "/forms",
+  },
+  {
+    title: "Account",
+    href: "/forms/account",
+  },
+  {
+    title: "Appearance",
+    href: "/forms/appearance",
+  },
+  {
+    title: "Notifications",
+    href: "/forms/notification",
+  },
+  {
+    title: "Display",
+    href: "/forms/display",
+  },
+]
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -19,11 +42,18 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <div>
-          <div>
-            {children}
-          </div>
-          </div>
+      <div className="hidden space-y-6 p-10 pb-16 md:block">
+        <div className="space-y-0.5">
+        <NavMenu/>  
+        </div>
+        <Separator className="my-6" />
+        <div className="flex flex-col space-y-8 lg:flex-row lg:space-x-12 lg:space-y-0">
+          <aside className="-mx-4 lg:w-1/5">
+            <SidebarNav items={sidebarNavItems} />
+          </aside>
+          <div className="flex-1 lg:max-w-2xl">{children}</div>
+        </div>
+      </div>
         </body>
     </html>
   );
