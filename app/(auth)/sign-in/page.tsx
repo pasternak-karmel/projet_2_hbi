465121b139ee72cde71f56c2f9937ad300163e76
pasternak-karmel <101138016+pasternak-karmel@users.noreply.github.com>
2024-08-08 10:20:49 +0100

    starting development

---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,12 @@
+export default function SignIn(){
+    return (
+        <div>
+            <h1>Sign In</h1>
+            <form>
+                <input type="email" placeholder="Email" />
+                <input type="password" placeholder="Password" />
+                <button type="submit">Sign In</button>
+            </form>
+        </div>
+    )
+}
