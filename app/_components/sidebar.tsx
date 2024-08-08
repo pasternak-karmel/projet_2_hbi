bfd97cf1d4172cd64fffb9273c7ca8b5e089092a
pasternak-karmel <101138016+pasternak-karmel@users.noreply.github.com>
2024-08-08 12:00:05 +0100

    preference and sign-up

---
+++ app/_components/sidebar.tsx
@@ -0,0 +1,7 @@
+export function  SideBar(){
+    return (
+        <div className="flex flex-col w-[400px] shadow">
+            SideBars
+        </div>
+    )
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { NavMenu } from "./_components/navbar";
+import { SideBar } from "./_components/sidebar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +18,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div>
+          <div>
+            {children}
+          </div>
+          </div>
+        </body>
     </html>
   );
 }
