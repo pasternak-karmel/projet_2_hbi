478f927ac5f5ac5ffce3b411c7639189e9321ade
berenger-bayer <somminberengersannibayer@gmail.com>
2024-08-22 12:10:20 -0500

    nav-menu and responsivity

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -26,6 +26,7 @@ import { Avatar, AvatarImage } from "./ui/avatar";
 import dynamic from "next/dynamic";
 import { cn } from "@/lib/utils";
 import SearchBar from "./SearchBar";
+import Menu from "./Menu";
 
 const CartModal = dynamic(() => import("./CartModal"), { ssr: false });
 
@@ -45,130 +46,177 @@ export function MainNav() {
 
   const counter = useMemo(() => 2, []);
 
-  return (
-    <div className="flex gap-4 items-center justify-between">
-      <CustomLink href="/">
-        <Button variant="ghost" className="p-0">
-          MARKETPLACE
-        </Button>
-      </CustomLink>
+ return (
+    <div className="h-20 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-32 relative">
+          {/* mobile */}
+          <div className="h-full flex items-center justify-between md:hidden">
+            <Link href="/">
+              <div className="text-2xl tracking-wide">MARKETPLACE</div>
+            </Link>
+            <Menu />
+          </div>
+          {/* grand ecran */}
+          <div className="hidden md:flex items-center justify-between gap-8 h-full">
+            {/* gauche */}
+            <div className="w-1/3 xl:w-1/2 flex items-center gap-12">
+                  <CustomLink href="/" className="flex items-center gap-3">
+                  <Image src="/logo.png" alt="" width={24} height={24} />
+                  <div className="text-2xl tracking-wide">
+                    <Button variant="ghost" className="text-2xl tracking-wide">
+                      MARKETPLACE
+                    </Button>
+                  </div>  
+                  </CustomLink>
+              </div>            
+             {/* droite */}
+              <div className="w-2/3 xl:w-1/2 flex items-center  gap-2">
+                        <NavigationMenu>
+                        <NavigationMenuList>
+                          <NavigationMenuItem>
+                            <NavigationMenuLink
+                              href="/categories"
+                              className={navigationMenuTriggerStyle()}
+                            >
+                              Categories
+                            </NavigationMenuLink>
+                          </NavigationMenuItem>
+                          <NavigationMenuItem>
+                            <NavigationMenuLink
+                              href="/All-Products"
+                              className={navigationMenuTriggerStyle()}
+                             >
+                              Articles
+                            </NavigationMenuLink>
+                          </NavigationMenuItem>
+                        </NavigationMenuList>
+                      </NavigationMenu>
 
-      <NavigationMenu>
-        <NavigationMenuList>
-          <NavigationMenuItem>
-            <NavigationMenuLink
-              href="/categories"
-              className={navigationMenuTriggerStyle()}
-            >
-              Categories
-            </NavigationMenuLink>
-          </NavigationMenuItem>
-          <NavigationMenuItem>
-            <NavigationMenuLink
-              href="/All-Products"
-              className={navigationMenuTriggerStyle()}
-            >
-              Articles
-            </NavigationMenuLink>
-          </NavigationMenuItem>
-        </NavigationMenuList>
-      </NavigationMenu>
+                      <SearchBar />
 
-      <SearchBar />
+                      {session ? (
+                        <div className="flex gap-2 items-center">
+                          <DropdownMenu>
+                            <DropdownMenuTrigger asChild>
+                              <Button variant="ghost" className="relative w-8 h-8 rounded-full">
+                                <Avatar className="w-8 h-8">
+                                  <AvatarImage
+                                    src={
+                                      session.user?.image ??
+                                      "https://source.boringavatars.com/marble/120"
+                                    }
+                                    alt={session.user?.name ?? ""}
+                                  />
+                                </Avatar>
+                              </Button>
+                            </DropdownMenuTrigger>
+                            <DropdownMenuContent className="w-56" align="end">
+                              <DropdownMenuLabel className="font-normal">
+                                <div className="flex flex-col space-y-1">
+                                  <div className="flex gap-2">
+                                    <div>
+                                      <Avatar className="w-8 h-8">
+                                        <AvatarImage
+                                          src={
+                                            session.user?.image ??
+                                            "https://source.boringavatars.com/marble/120"
+                                          }
+                                          alt={session.user?.name ?? ""}
+                                        />
+                                      </Avatar>
+                                    </div>
+                                    <div>
+                                      <p className="text-sm font-medium leading-none">
+                                        {session.user?.name}
+                                      </p>
+                                      <p className="text-xs leading-none text-muted-foreground">
+                                        {session.user?.email}
+                                      </p>
+                                    </div>
+                                  </div>
+                                </div>
+                              </DropdownMenuLabel>
+                              <DropdownMenuItem onClick={() => signOut()}>
+                                SignOut
+                              </DropdownMenuItem>
+                            </DropdownMenuContent>
+                          </DropdownMenu>
+                        </div>
+                      ) : (
+                        <Button onClick={() => signIn()}>Inscription/Connexion</Button>
+                      )}
 
-      {session ? (
-        <div className="flex gap-2 items-center">
-          <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-              <Button variant="ghost" className="relative w-8 h-8 rounded-full">
-                <Avatar className="w-8 h-8">
-                  <AvatarImage
-                    src={
-                      session.user?.image ??
-                      "https://source.boringavatars.com/marble/120"
-                    }
-                    alt={session.user?.name ?? ""}
-                  />
-                </Avatar>
-              </Button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent className="w-56" align="end">
-              <DropdownMenuLabel className="font-normal">
-                <div className="flex flex-col space-y-1">
-                  <div className="flex gap-2">
-                    <div>
-                      <Avatar className="w-8 h-8">
-                        <AvatarImage
-                          src={
-                            session.user?.image ??
-                            "https://source.boringavatars.com/marble/120"
-                          }
-                          alt={session.user?.name ?? ""}
-                        />
-                      </Avatar>
-                    </div>
-                    <div>
-                      <p className="text-sm font-medium leading-none">
-                        {session.user?.name}
-                      </p>
-                      <p className="text-xs leading-none text-muted-foreground">
-                        {session.user?.email}
-                      </p>
-                    </div>
-                  </div>
-                </div>
-              </DropdownMenuLabel>
-              <DropdownMenuItem onClick={() => signOut()}>
-                SignOut
-              </DropdownMenuItem>
-            </DropdownMenuContent>
-          </DropdownMenu>
-        </div>
-      ) : (
-        <Button onClick={() => signIn()}>Inscription/Connexion</Button>
-      )}
+                      <div className="hidden md:flex items-center justify-between gap-8 h-full">
+                        {/* Autres éléments... */}
 
-      {pathname !== "/produit" && (
-        <Link href="/produit">
-          <Button>Publier un produit</Button>
-        </Link>
-      )}
+                        <div className="w-2/3 xl:w-1/2 flex items-center justify-between gap-8">
+                          {/* Autres éléments... */}
 
-      <div
-        className="relative cursor-pointer"
-        onClick={() => setIsCartOpen((prev) => !prev)}
-      >
-        <Image src="/cart.png" alt="" width={22} height={22} />
-        <div className="absolute -top-4 -right-4 w-6 h-6 bg-red-400 rounded-full text-white text-sm flex items-center justify-center">
-          {counter}
-        </div>
+                          {pathname !== "/produit" && (
+                            <>
+                              {/* Afficher le bouton pour les écrans plus larges */}
+                              <div className="hidden lg:block">
+                                <Link href="/produit">
+                                  <Button>Publier un produit</Button>
+                                </Link>
+                              </div>
+
+                              {/* Afficher l'image pour les écrans de 1000x652 */}
+                              <div className="block lg:hidden">
+                                <Link href="/produit">
+                                  <Image
+                                    src="/publier.png"
+                                    alt="Publier un produit"
+                                    width={24}
+                                    height={24}
+                                    className="w-6 h-6"
+                                  />
+                                </Link>
+                              </div>
+                            </>
+                          )}
+
+                          {/* Autres éléments... */}
+                        </div>
+                      </div>
+
+
+                      {/* <div
+                        className="relative cursor-pointer"
+                        onClick={() => setIsCartOpen((prev) => !prev)}
+                      >
+                        <Image src="/cart.png" alt="" width={22} height={22} />
+                        <div className="absolute -top-4 -right-4 w-6 h-6 bg-red-400 rounded-full text-white text-sm flex items-center justify-center">
+                          {counter}
+                        </div>
+                      </div>
+
+                      {isCartOpen && <CartModal />} */}
+            </div>
+          </div>     
       </div>
-
-      {isCartOpen && <CartModal />}
-    </div>
   );
 }
 
-const ListItem = React.forwardRef<
-  React.ElementRef<"a">,
-  React.ComponentPropsWithoutRef<"a">
->(({ className, title, children, ...props }, ref) => (
-  <li>
-    <NavigationMenuLink asChild>
-      <a
-        ref={ref}
-        className={cn(
-          "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
-          className
-        )}
-        {...props}
-      >
-        <div className="text-sm font-medium leading-none">{title}</div>
-        <p className="text-sm leading-snug line-clamp-2 text-muted-foreground">
-          {children}
-        </p>
-      </a>
-    </NavigationMenuLink>
-  </li>
-));
-ListItem.displayName = "ListItem";
+// const ListItem = React.forwardRef<
+//   React.ElementRef<"a">,
+//   React.ComponentPropsWithoutRef<"a">
+// >(({ className, title, children, ...props }, ref) => (
+//   <li>
+//     <NavigationMenuLink asChild>
+//       <a
+//         ref={ref}
+//         className={cn(
+//           "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
+//           className
+//         )}
+//         {...props}
+//       >
+//         <div className="text-sm font-medium leading-none">{title}</div>
+//         <p className="text-sm leading-snug line-clamp-2 text-muted-foreground">
+//           {children}
+//         </p>
+//       </a>
+//     </NavigationMenuLink>
+//   </li>
+// ));
+// ListItem.displayName = "ListItem";
