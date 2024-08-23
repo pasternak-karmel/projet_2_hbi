b626e0f3fa3d108a93998457d61eec6ebce17d75
berenger-bayer <somminberengersannibayer@gmail.com>
2024-08-23 05:41:02 -0500

    dinamic-products

--- components/ProductList.tsx
+++ components/ProductList.tsx
@@ -1,56 +1,38 @@
+"use client"
+
 import Image from "next/image";
 import Link from "next/link";
-//import Pagination from "./Pagination";
-import { berengerproduit, produitItems } from "@/types";
+import { useQuery } from "@tanstack/react-query";
+import Loader from "@/components/Loader";
 
-const PRODUCT_PER_PAGE = 8;
+const PRODUCT_PER_PAGE = 4;
 
-export default async function ProductList({
-  categoryId,
-  limit,
-  searchParams,
-}:{
-  categoryId: string;
-  limit: number;
-  searchParams?: any;
-}
-)
-  
-{
+export default function ProductList({
+  // categoryId,
+  // limit = PRODUCT_PER_PAGE,
+  // searchParams,
+}: {
+  // categoryId: string;
+  // limit?: number;
+  // searchParams?: any;
+}) {
+  const { isLoading, error, data } = useQuery({
+    queryKey: ["Acceuilproduit"],
+    queryFn: () =>
+      fetch(`/api/getnewproduct`).then((res) => res.json()),
+      // fetch(`/api/getnewproduct?categoryId=${categoryId}&limit=${limit}`).then((res) => res.json()),
+  });
 
-  // const productQuery = wixClient.products
-  //   .queryProducts()
-  //   .startsWith("name", searchParams?.name || "")
-  //   .eq("collectionIds", categoryId)
-  //   .hasSome(
-  //     "productType",
-  //     searchParams?.type ? [searchParams.type] : ["physical", "digital"]
-  //   )
-  //   .gt("priceData.price", searchParams?.min || 0)
-  //   .lt("priceData.price", searchParams?.max || 999999)
-  //   .limit(limit || PRODUCT_PER_PAGE)
-  //   .skip(
-  //     searchParams?.page
-  //       ? parseInt(searchParams.page) * (limit || PRODUCT_PER_PAGE)
-  //       : 0
-  //   );
-  // if (searchParams?.sort) {
-  //   const [sortType, sortBy] = searchParams.sort.split(" ");
+  if (isLoading) return <Loader />;
+  if (error) return <div>Une erreur s'est produite: {error.message}</div>;
 
-  //   if (sortType === "asc") {
-  //     productQuery.ascending(sortBy);
-  //   }
-  //   if (sortType === "desc") {
-  //     productQuery.descending(sortBy);
-  //   }
-  // }
-
-  // const res = await productQuery.find();
+  if (data.articles.length === 0) {
+    return <div>Aucun produit disponible pour le moment.</div>;
+  }
 
   return (
     <div className="mt-12 flex gap-x-8 gap-y-16 justify-between flex-wrap">
-      {/* {res.items.map((product: products.Product) => ( */}
-      {berengerproduit.map((product) => (
+      {data.articles.map((product: any) => (
         <Link
           href={"/" + product.id}
           className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]"
@@ -59,16 +41,15 @@ export default async function ProductList({
           <div className="relative w-full h-80">
             <Image
               src={product.image || "/product.png"}
-              alt=""
+              alt={product.nom}
               fill
               sizes="25vw"
-              className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity easy duration-500"
+              className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity ease duration-500"
             />
             {product.image && (
               <Image
-              // src={product.media?.items[1]?.image?.url || "/product.png"}
-              src={product.image || "/product.png"}
-                alt=""
+                src={product.image || "/product.png"}
+                alt={product.nom}
                 fill
                 sizes="25vw"
                 className="absolute object-cover rounded-md"
@@ -77,32 +58,13 @@ export default async function ProductList({
           </div>
           <div className="flex justify-between">
             <span className="font-medium">{product.nom}</span>
-            <span className="font-semibold">${product.prix}</span>
+            <span className="font-semibold">{product.prix} XOF</span>
           </div>
-          {/* {product.additionalInfoSections && (
-            <div
-              className="text-sm text-gray-500"
-              dangerouslySetInnerHTML={{
-                __html: DOMPurify.sanitize(
-                  product.additionalInfoSections.find(
-                    (section: any) => section.title === "shortDesc"
-                  )?.description || ""
-                ),
-              }}
-            ></div>
-          )} */}
           <button className="rounded-2xl ring-1 ring-lama text-lama w-max py-2 px-4 text-xs hover:bg-black hover:text-white">
-            Ajouter au panier 
+            Ajouter au panier
           </button>
         </Link>
       ))}
-      {/* {searchParams?.cat || searchParams?.name ? (
-        <Pagination
-          currentPage={res.currentPage || 0}
-          hasPrev={res.hasPrev()}
-          hasNext={res.hasNext()}
-        />
-      ) : null} */}
     </div>
   );
-};
+}
